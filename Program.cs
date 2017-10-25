c8d9b2ec92372ff5c328bfa1ebadcf42ceacf71e
ted <qpwo@mail.ru>
2017-10-26 00:27:45 +0700

    Коммент и writeline

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace Home_Finance_02
     {
         static void Main(string[] args)
         {
+            // Добавлен комментарий
+            Console.WriteLine();
             HF_BD MyBD = new HF_BD();
             MainWindow W = new MainWindow();
             W.Start(MyBD);
